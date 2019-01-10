86065dea430de0d9a56d312cb1ed985edc985d84
Evgeniy Solkin <44978047+solkinei@users.noreply.github.com>
2019-01-10 22:14:45 +0300

    Update main3.cpp

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,20 +1,16 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include "task3.h"
 #include <stdio.h>
 #include <locale.h>
-
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-
 	char buf[128];
 	double angle;
 	char type;
 	type = 'D';
 	type = 'R';
 
-	printf("Введите угол в градусах __.__D или радианах __.__R: \n");
+	printf("Р’РІРµРґРёС‚Рµ СѓРіРѕР» РІ РіСЂР°РґСѓСЃР°С… __.__D РёР»Рё СЂР°РґРёР°РЅР°С… __.__R: \n");
 	scanf("%lf%c", &angle, &type);
 	convert(buf, angle, type);
 	puts(buf);
