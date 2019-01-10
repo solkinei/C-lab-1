a958d0d95e59aa1c5d2d1836ccc50a6c94042978
Evgeniy Solkin <44978047+solkinei@users.noreply.github.com>
2019-01-10 22:40:08 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,15 +1,14 @@
+#define _USE_MATH_DEFINES 
+#include <math.h>
 #include <stdio.h>
-const float PI = 3.14159;
-char * convert(char buf[], double angle, char type)
-{
-	if (type == 'D')
-	{
-		sprintf(buf, "Угол в %lf%c равен углу %lfR", angle, type, angle * PI / 180);
-	}
-	else
-	{
-		sprintf(buf, "Угол в %lf%c равен углу %lfD", angle, type, angle * 180 / PI);
-	}
 
+char * convert(char buf[], double angle, char type)
+{		
+	if (type == 'D')
+		sprintf(buf, "%f%c", angle * M_PI / 180, 'R');
+	else if (type == 'R')
+		sprintf(buf, "%f%c", angle * 180 / M_PI, 'D');
+	else
+		sprintf(buf, "Incorrect!");
 	return buf;
-}
+} 
