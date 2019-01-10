2824a1aefcc3bdbd65618d159ba236bc0e6deab9
Evgeniy Solkin <44978047+solkinei@users.noreply.github.com>
2019-01-10 22:32:41 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include "task1.h"
-#include <locale.h>
+
 int main()
 {
-	setlocale(LC_ALL, "Rus");
 	char gender = 0;
 	float height = 0;
 	float weight = 0;
 
-	printf("Укажите ваш пол (м или ж): ");
-	scanf("%c", &gender);
-	printf("Укажите ваш рост в см: ");
-	scanf("%f", &height);
-	printf("Укажите ваш вес в кг: ");
-	scanf("%f", &weight);
+		printf("Enter your gender 'm' or 'w':");
+		scanf("%c", &gender);
+
+		printf("Enter your height:");
+		scanf("%f", &height);
+
+		printf("Enter your weight:");
+		scanf("%f", &weight);
 
 	int result = getRecommendation(gender, height, weight);
-	if (result == 1)
-		printf("Вам нужно сбросить вес!\n");
-	else if (result == 0)
-		printf("Ваш вес нормальный!\n");
-	else if (result == -1)
-		printf("Вам нужно набрать весь!\n");
-	return 0;
-}
+
+		if (result == 0)
+		printf("Weight good\n");
+		if (result == 1)
+		printf("Weight bad\n");
+		if (result == -1)
+		printf("Weight small\n");
+		return 0;
+	} 
