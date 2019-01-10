36ee1954c06e5e54b3a38ff0b626b58017d3d478
Evgeniy Solkin <44978047+solkinei@users.noreply.github.com>
2019-01-10 22:34:36 +0300

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include "task2.h"
-#include <locale.h>
+void clean_stdin()
+{
+	int c;
+	do
+		c = getchar();
+	while (c != '\n'&&c != EOF);
+}
+
+
 int main()
 {
-	setlocale(LC_ALL, "Rus");
-	int min = 0;
 	int hour = 0;
+	int min = 0;
 	int sec = 0;
-	printf("Введите время в форме чч:мм:сс:");
-	scanf("%d:%d:%d", &hour, &min, &sec);
+	while (1)
+	{
+
+		printf("Enter Hours:Minutes:Seconds:");
+		int result=scanf("%d:%d:%d", &hour, &min, &sec);
+		if (result !=3|| hour < 0 || hour >= 24 || min < 0 || min >= 60 || sec < 0 || sec >= 60)
+		{
+			printf("Uncorrect time!");
+			clean_stdin();
+		}
+		else
+			break;
+	}
 	printf(greet(hour, min));
+
 	return 0;
-}
+} 
