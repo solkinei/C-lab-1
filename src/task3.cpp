2981ff3e78fb0a6ce2cc2e94b9b1f8930dcf16a4
Evgeniy Solkin <44978047+solkinei@users.noreply.github.com>
2019-01-10 22:13:34 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,17 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 const float PI = 3.14159;
-
 char * convert(char buf[], double angle, char type)
 {
 	if (type == 'D')
 	{
-		sprintf(buf, "Угол в %lf%c равен углу %lfR", angle, type, angle * PI / 180);
+		sprintf(buf, "РЈРіРѕР» РІ %lf%c СЂР°РІРµРЅ СѓРіР»Сѓ %lfR", angle, type, angle * PI / 180);
 	}
 	else
 	{
-		sprintf(buf, "Угол в %lf%c равен углу %lfD", angle, type, angle * 180 / PI);
+		sprintf(buf, "РЈРіРѕР» РІ %lf%c СЂР°РІРµРЅ СѓРіР»Сѓ %lfD", angle, type, angle * 180 / PI);
 	}
 
 	return buf;
