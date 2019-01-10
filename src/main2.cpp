6fb5037898897525774971c970ebb70899f687a2
Evgeniy Solkin <44978047+solkinei@users.noreply.github.com>
2019-01-10 22:09:14 +0300

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -4,11 +4,11 @@
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	int tmin = 0;
-	int thour = 0;
-	int tsec = 0;
+	int min = 0;
+	int hour = 0;
+	int sec = 0;
 	printf("Введите время в форме чч:мм:сс:");
-	scanf("%d:%d:%d", &thour, &tmin, &tsec);
-	printf(greet(thour, tmin));
+	scanf("%d:%d:%d", &hour, &min, &sec);
+	printf(greet(hour, min));
 	return 0;
 }
